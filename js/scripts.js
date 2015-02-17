1697ea0346b0ea42a06d5b8eb1ba2dc74c008b21
Gabe Finch and Lee Jorgensen <gabefinch@gmail.com>
2015-02-17 15:53:36 -0800

    Round price, add styling

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
   var destination = $("input#destination").val();
   var newPackage = { itemName: itemName, itemWeight: itemWeight, destination: destination,
                       price: price = function(){
-                          return (this.itemWeight * 0.45);
+                          return (this.itemWeight * 0.45).toFixed(2);
                       }
     };
 
     $("ul#new_packages").append("<li>" + "Shipping cost: " + newPackage.itemName + " " + "$" + newPackage.price() + "</li>")
-
+    $('#results').show();
     var itemName = $("input#item_name").val("");
     var itemWeight = $("input#item_weight").val("");
     var destination = $("input#destination").val("");
